e2ed38af585b1b748758119036db5699bb74b29a
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-09-09 14:55:16 -0300

    Install express

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+const express = require('express')
+
+const app = express()
+/*
+GET= Buscar 
+DELETE = Deletar 
+PUT = Alterar 
+POST = Criar
+*/
+
+app.get('', (request,response)=>{
+    return response.send('ok')
+})
+
+app.listen(3000, ()=> {
+    console.log('Serviço rodando  na porta 3000')
+})
