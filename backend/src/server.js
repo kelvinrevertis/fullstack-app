a7c5ccf543760bbc1b06af7fac44d4eca373d9d5
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-09-12 15:27:07 -0300

    Teste
    Teste

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,5 +2,5 @@ const app = require('./app')
 import app from "./app.js"
 
 app.listen(3000, ()=> {
-    console.log('Serviço rodando  na porta 3000')
+    console.log('Servico rodando  na porta 3000')
 })
